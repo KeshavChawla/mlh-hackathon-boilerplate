ba0ccc0b7955eb0464575f971f77ec1a1082c3db
Lokhin Wu <lokhin.wu@live.com>
2017-02-01 20:42:29 -0500

    alert

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-    var faqHeight = $('.faq').height();
-    var lineHeight = $('#linecount-start').height();
-    var lineCount = faqHeight / lineHeight;
-    alert(lineCount);
-    for (i = 1; i < lineCount; i++) {
-        var newNode = document.createElement('li');
-        var innerContent = document.createTextNode(i);
-        newNode.appendChild(innerContent);
-        document.getElementById('sidebar').appendChild(newNode);
-    }
-});
+// $(document).ready(function() {
+//     var faqHeight = $('.faq').height();
+//     var lineHeight = $('#linecount-start').height();
+//     var lineCount = faqHeight / lineHeight;
+    alert("hello");
+//     for (i = 1; i < lineCount; i++) {
+//         var newNode = document.createElement('li');
+//         var innerContent = document.createTextNode(i);
+//         newNode.appendChild(innerContent);
+//         document.getElementById('sidebar').appendChild(newNode);
+//     }
+// });
