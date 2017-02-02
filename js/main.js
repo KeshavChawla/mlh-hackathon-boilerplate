902937db712c2a882498be7cd159c11bd72823a2
Lokhin Wu <lokhin.wu@live.com>
2017-02-01 20:39:47 -0500

    sidebar

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
-var faqHeight = $('.faq').height();
-var lineHeight = $('#linecount-start').height();
-var lineCount = faqHeight / lineHeight;
+    var faqHeight = $('.faq').height();
+    var lineHeight = $('#linecount-start').height();
+    var lineCount = faqHeight / lineHeight;
 
-for (i = 1; i < lineCount; i++) {
-var newNode = document.createElement('li');
-var innerContent = document.createTextNode(i);
-newNode.appendChild(innerContent);
-document.getElementById('sidebar').appendChild(newNode);
-}
-});
-});
+    for (i = 1; i < lineCount; i++) {
+        var newNode = document.createElement('li');
+        var innerContent = document.createTextNode(i);
+        newNode.appendChild(innerContent);
+        document.getElementById('sidebar').appendChild(newNode);
+    }
 });
